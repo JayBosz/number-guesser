4e2230690a2a19ee1e300d400a162d2c692413e3
Jabeur Alimi <54047277+JayBosz@users.noreply.github.com>
2023-06-25 05:42:40 -0400

    Update Number guesser.py

--- Number guesser.py
+++ Number guesser.py
@@ -5,6 +5,9 @@ limite = input("Please select the maximum value number")
 if limite.isdigit():
     limite = int(limite)
 
+    if limite <= 0:
+        print("please choose a number that is bigger than 0")
+        quit()
 else:
     print("Please enter a valid number next time")
     quit()
